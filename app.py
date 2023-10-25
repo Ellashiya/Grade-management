57e7099fd6e4e53b1cd2d2bd4039e92e01603ff1
MinKyoung <106471509+Ellashiya@users.noreply.github.com>
2023-10-25 15:03:41 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import sys
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
-def home():
-    return '''
-    <h1>h1 제목</h1>
-    <p>p 본문 </p>
-    <a href="https://flask.palletsprojects.com">Flask 홈페이지 바로가기</a>
-    '''
+def main():
+    return render_template("index.html")
 
-@app.route('/user/<user_name>/<int:user_id>')
-def user(user_name, user_id):
-    return f'Hello, {user_name}({user_id})!'
+@app.route('/planner/month')
+def planner_month():
+    return render_template('layout-planner-month.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
