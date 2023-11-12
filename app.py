3bf465e5f5af132b89fb4ad1f1a8ee0d235b5c0d
MinKyoung <106471509+Ellashiya@users.noreply.github.com>
2023-11-12 16:00:31 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, render_template, make_response
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_cors import CORS
+from db_model.models import db
 from guide_view import guide # (블루프린트)guide_view/guide.py 임포트
 import os
 
@@ -9,9 +10,27 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__, static_url_path='/static')
 CORS(app)
-app.secret_key = 'load_management'
 
-app.register_blueprint(guide.guide_app, url_prefix='/guide') # 블루프린트 등록
+#현재 있는 파일의 디렉토리 절대 경로
+basdir = os.path.abspath(os.path.dirname(__file__))
+#위 경로에 db파일 생성
+dbfile = os.path.join(basdir, 'db.sqlite')
+
+#사용할 DB URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+#Commint 실행(DB 반영)
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+#수정사항에 대한 TRACK
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#SECRET_Key
+app.config['SECRET_KEY'] = 'load_management'
+
+db.init_app(app)
+db.app = app
+db.create_all()
+
+app.register_blueprint(guide.guide_view, url_prefix="")
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.session_protection = 'strong'
@@ -21,11 +40,11 @@ login_manager.session_protection = 'strong'
 #     return User.get(user_id)
 
 # 로그인하지 않은 유저가 허용되지 않은 부분에 접근할 경우
-@login_manager.unauthorized_handler
-def unauthorized():
-    return make_response(jsonify(success=False), 401) 
+#@login_manager.unauthorized_handler
+#def unauthorized():
+#    return make_response(jsonify(success=False), 401) 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='1114') 
-    # http://localhost:1114/guide/test
+    app.run(host='0.0.0.0', port='1114', debug=True) 
+    # http://localhost:1114/guide/
