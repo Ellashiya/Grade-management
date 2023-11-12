1e72524ffe40a87636e87ff4d4c180e0282ddb0b
quartzs2 <csh001231@gmail.com>
2023-11-13 00:31:42 +0900

    user management 수정

--- guide_control/user_mgmt.py
+++ guide_control/user_mgmt.py
@@ -4,8 +4,9 @@ from db_model.sqlite import conn_sqlitedb
 
 class User(UserMixin):
     
-    def __init__(self, user_id):
+    def __init__(self, user_id, user_email):
         self.id = user_id
+        self.user_email = user_email
         
     def get_id(self):
         return str(self.id)
@@ -14,52 +15,48 @@ class User(UserMixin):
     # 작성 필요
     @staticmethod
     def get(user_id):
-        sqlite3_db = conn_sqlitedb()
-        db_cursor = sqlite3_db.cursor()
+        sqlite_db = conn_sqlitedb()
+        db_cursor = sqlite_db.cursor()
         sql = 'sql문 작성'
         db_cursor.execute(sql)
         user = db_cursor.fetchone()
         if not user:
             return None
         
-        user = User(user_id=user[0])
+        user = User(user_id=user[0], user_email=user[1])
         return user
     
     @staticmethod
-    def find(user_id):
-        sqlite3_db = conn_sqlitedb()
-        db_cursor = sqlite3_db.cursor()
+    def find(user_email):
+        sqlite_db = conn_sqlitedb()
+        db_cursor = sqlite_db.cursor()
         sql = 'sql문 작성'
         db_cursor.execute(sql)
         user = db_cursor.fetchone()
         if not user:
             return None
         
-        user = User(user_id=user[0])
+        user = User(user_id=user[0], user_email=user[1])
         return user
     
     @staticmethod
-    def create(user_id):
-        sqlite3_db = conn_sqlitedb()
-        db_cursor = sqlite3_db.cursor()
-        sql = 'sql문 작성'
-        db_cursor.execute(sql)
-        user = db_cursor.fetchone()
-        if not user:
-            return None
+    def create(user_email, user_id):
+        user = User.find(user_email)
+        if user == None:
+            sqlite_db = conn_sqlitedb()
+            db_cursor = sqlite_db.cursor()
+            sql = 'sql문 작성'
+            db_cursor.execute(sql)
+            sqlite_db.commit()
+            return User.find(user_email)
+        else:
+            return user
         
-        user = User(user_id=user[0])
-        return user
-    
     @staticmethod
     def delete(user_id):
-        sqlite3_db = conn_sqlitedb()
-        db_cursor = sqlite3_db.cursor()
+        sqlite_db = conn_sqlitedb()
+        db_cursor = sqlite_db.cursor()
         sql = 'sql문 작성'
-        db_cursor.execute(sql)
-        user = db_cursor.fetchone()
-        if not user:
-            return None
-        
-        user = User(user_id=user[0])
-        return user
+        deleted = db_cursor.execute(sql)
+        sqlite_db.commit()
+        return deleted
